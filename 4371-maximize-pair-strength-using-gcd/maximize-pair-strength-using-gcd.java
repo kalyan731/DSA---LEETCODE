fcd140a0863d48f6bb77573d1ff5320d19944e7e
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-08-24 11:12:04 +0530

    Time: 999 ms (31.51%) | Memory: 46.9 MB (77.49%) - LeetSync

---
+++ 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long maxPairStrength(int[] nums) {
+        long max = Long.MIN_VALUE;
+        
+        for(int i = 0; i < nums.length;i++){
+            for(int j = i+1;j < nums.length;j++){
+                long sum = (long)nums[i]*nums[j];
+                long g = gcd(nums[i],nums[j]);
+                long val = sum/(g*g);
+                max = Math.max(max,val);
+            }
+        }
+        return max;
+    }
+    public long gcd(long a,long b){
+        a = Math.abs(a);
+        b = Math.abs(b);
+      
+        while(b != 0){
+            long temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+}
