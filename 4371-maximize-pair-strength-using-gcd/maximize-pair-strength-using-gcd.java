330b4ee53a84d529ce58f68596cfb70887fb2bdf
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-08-24 11:21:27 +0530

    Time: 980 ms (41.64%) | Memory: 46.7 MB (92.18%) - LeetSync

--- 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.java
+++ 4371-maximize-pair-strength-using-gcd/maximize-pair-strength-using-gcd.java
@@ -12,9 +12,7 @@ class Solution {
         return max;
     }
     public long gcd(long a,long b){
-        a = Math.abs(a);
-        b = Math.abs(b);
-      
+       
         while(b != 0){
             long temp = b;
             b = a%b;
