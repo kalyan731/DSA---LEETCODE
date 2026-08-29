e3534e859e0d6fcb8802606472b192dbf7b4e9d0
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-08-29 12:36:18 +0530

    Time: 2 ms (96.67%) | Memory: 44 MB (46.10%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,16 +1,13 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int  i = num1.length()-1;
-        int j = num2.length()-1;
+        int  i = num1.length()-1,j = num2.length()-1;;
         StringBuilder sb = new StringBuilder("");
-
         int carry = 0;
       
         while(i >= 0 || j >= 0 || carry != 0){
             int a = (i >= 0)?num1.charAt(i) -'0':0;
             int b = (j >= 0)?num2.charAt(j) -'0':0;
             int res = a + b + carry;
-           
             sb.append(res%10);
             carry = res/10;
             i--;
