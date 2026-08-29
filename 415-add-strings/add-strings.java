71152952ae46d96aa02d99d20939dba7eebd96a7
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-08-29 12:34:53 +0530

    Time: 3 ms (22.96%) | Memory: 43.7 MB (76.36%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int  i = num1.length()-1;
+        int j = num2.length()-1;
+        StringBuilder sb = new StringBuilder("");
+
+        int carry = 0;
+      
+        while(i >= 0 || j >= 0 || carry != 0){
+            int a = (i >= 0)?num1.charAt(i) -'0':0;
+            int b = (j >= 0)?num2.charAt(j) -'0':0;
+            int res = a + b + carry;
+           
+            sb.append(res%10);
+            carry = res/10;
+            i--;
+            j--;
+            
+        }
+        return sb.reverse().toString();
+        
+    }
+}
