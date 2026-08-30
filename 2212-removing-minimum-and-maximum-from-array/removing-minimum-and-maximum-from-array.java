1eaac9dae92e441974d61272d44ebee9dcb1563b
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-08-30 15:36:40 +0530

    Time: 3 ms (61.82%) | Memory: 86.6 MB (60.18%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -17,6 +17,8 @@ class Solution {
         }
         int l = Math.min(mini,maxi);
         int r = Math.max(mini,maxi) ;
+        int f = r+1;
+        int b = n-l;
         return Math.min(Math.min(r+1,n-l),l+1+n-r);
 
         
