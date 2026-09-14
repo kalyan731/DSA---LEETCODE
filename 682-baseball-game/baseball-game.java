1fcaef9bfd88e601f12643d2184c7610b3015c50
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-09-14 23:05:51 +0530

    Time: 3 ms (79.51%) | Memory: 43.3 MB (82.75%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -4,12 +4,9 @@ class Solution {
         int n = arr.length;
         
         for(int i = 0; i < n;i++){
-            if(arr[i].equals("C")){
-                st.pop();
-            }else if(arr[i].equals("D")){
-                st.push((st.peek())*2);
-
-            }else if(arr[i].equals("+")){
+            if(arr[i].equals("C")) st.pop();
+            else if(arr[i].equals("D")) st.push((st.peek())*2);
+            else if(arr[i].equals("+")){
                 int y = st.pop();
                 int x = y + st.peek();
                 st.push(y);
