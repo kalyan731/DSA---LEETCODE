03743c5d59f590f349c1734c2e5523b915d74045
Kuruva Kalyan <kulthappa1980kalyan@gmail.com>
2026-09-19 00:59:01 +0530

    Time: 4 ms (95.41%) | Memory: 44.9 MB (48.68%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        for (String part : path.split("/")) {
+            if (part.isEmpty() || part.equals(".")) {
+                continue;
+            }
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pollLast();
+                }
+            } else {
+                stack.addLast(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String dir : stack) {
+            sb.append("/").append(dir);
+        }
+        return sb.toString();
+
+        
+    }
+}
